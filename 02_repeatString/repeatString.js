ce5225f09d17671d5decc6fd0c23d4d8b65c1497
RanaAbdulrhman <RanaAbdulrhmanD@gmail.com>
2022-06-15 18:47:19 +0300

    solving the first three challenges

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if (num >=0) { 
+        let full = '';
+        for(let i=0; i<num ; i++){
+            full += word
+        }
+        return full;
+    }
+    else{
+        return "ERROR";
+    }
 };
-
+repeatString
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let LettersArray = word.split("");
+    let reversedArrray = [];
+    let l = "";
+    while(!LettersArray.length==0){ 
+        l = LettersArray.pop();
+        reversedArrray.push(l);
+    }
+    return reversedArrray.join("");
 };
 
 // Do not edit below this line
