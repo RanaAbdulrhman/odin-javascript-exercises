dc5ae14bc0d0756e162868434c5f48b7b9cb7f49
RanaAbdulrhman <RanaAbdulrhmanD@gmail.com>
2022-06-15 21:02:12 +0300

    solution for forth and fifth challenges

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
 const removeFromArray = function() {
-
+    let list = arguments[0];
+    let index = -1; 
+    for(let i= 1; i< arguments.length; i++){
+        index = list.indexOf(arguments[i]); 
+        if(index>=0)
+            list.splice(index, 1);
+    }
+    return list;
 };
 
+console.log(removeFromArray([1, 2, 3, 4], 3))
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    if(typeof num1 == 'number' && typeof num2 == 'number')
+        if(num1 >= 0 && num2 >=0){
+            if (num1>num2){
+                let z = num1;
+                num1 = num2;
+                num2 = z;
+            }
+            let sum = 0; 
+            for(let i = num1; i<= num2; i++){
+            sum = sum + i;
+            }
+            return sum;
+        }
+    return 'ERROR';   
 };
 
 // Do not edit below this line
